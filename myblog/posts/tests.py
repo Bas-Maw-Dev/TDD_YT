4a598909e266b94ddcf5420450cfded70ed41ab9
Bas Maw <115073747+Bas-Maw-Dev@users.noreply.github.com>
2024-04-12 16:20:46 +0100

    test:RED - string representation

--- myblog/posts/tests.py
+++ myblog/posts/tests.py
@@ -7,3 +7,7 @@ class PostModelTest(TestCase):
   def test_post_model_exists(self):
     posts = Post.objects.count()
     self.assertEqual(posts, 0)
+    
+  def test_string_representation(self):
+    post = Post(title="title")
+    self.assertEqual(str(post), post.title)
