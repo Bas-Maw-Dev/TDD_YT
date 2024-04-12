71636d02c7c660c06fa8ef5d9db126a8c98b5f8d
Bas Maw <115073747+Bas-Maw-Dev@users.noreply.github.com>
2024-04-12 16:02:43 +0100

    test: RED - post model exists

--- myblog/posts/tests.py
+++ myblog/posts/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+class PostModelTest(TestCase):
+  def test_post_model_exists(self):
+    posts = Post.objects.all()
+    self.assertTrue(len(posts) > 0)
